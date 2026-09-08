f3c8026a83cf82e1aa33fc6cf11d319ca3c44b23
Kevin Song <kevinmgls@al.insper.edu.br>
2026-09-08 10:10:05 -0300

    teste de id especifico

--- test_api.py
+++ test_api.py
@@ -82,3 +82,35 @@ def test_listar_imoveis_com_dados (mock_conectar_banco, client):
     mock_cursor.fetchall.assert_called_once()
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
+
+@patch("api.conectar_banco")
+def test_imovel_id_especifico(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.fetchone.return_value = (1, 'Nicole Common', 'Travessa', 'Lake Danielle', 'Judymouth', '85184', 'casa em condominio', 488423.52, '2017-07-29')
+
+    mock_conectar_banco.return_value = mock_conn
+    response = client.get("/imoveis/1")
+
+    assert response.status_code == 200
+    assert response.get_json() =={
+            "id": 1,
+            "logradouro": "Nicole Common",
+            "tipo_logradouro": "Travessa",
+            "bairro": "Lake Danielle",
+            "cidade": "Judymouth",
+            "cep": "85184",
+            "tipo": "casa em condominio",
+            "valor": 488423.52,
+            "data_aquisicao": "2017-07-29"
+        }
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT * FROM imoveis WHERE id = ?",
+        (1,)
+    )
+    mock_cursor.fetchone.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
