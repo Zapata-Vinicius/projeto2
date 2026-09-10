85244201474198e298027849e31040f5d0543ba4
Kevin Song <kevinmgls@al.insper.edu.br>
2026-09-10 16:27:29 -0300

    teste buscar por cidade

--- test_api.py
+++ test_api.py
@@ -352,13 +352,79 @@ def test_buscar_imoveis_tipo_erro(mock_conectar_banco, client):
     mock_cursor.fetchall.return_value = []
 
     response = client.get("/imoveis?tipo=castelo")
-    assert response.status_code == 404
-    assert response.get_json() == {"erro": "nenhum imóvel encontrado"}
+    assert response.status_code == 200
+    assert response.get_json() == []
 
     mock_cursor.execute.assert_called_once_with(
         "SELECT * FROM imoveis WHERE tipo = %s",
         ("castelo",)
     )
+    mock_cursor.fetchall.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+@patch("api.conectar_banco")
+def test_buscar_imoveis_cidade(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_conectar_banco.return_value = mock_conn
+    mock_cursor.fetchall.return_value =[
+        (
+            1,
+            "Nicole Common",
+            "Travessa",
+            "Lake Danielle",
+            "Judymouth",
+            "85184",
+            "casa",
+            488423.52,
+            "2017-07-29"
+        )
+    ]
+    response = client.get("/imoveis?cidade=Judymouth")
+    assert response.status_code == 200
+    assert response.get_json() == [
+        {
+            "id": 1,
+            "logradouro": "Nicole Common",
+            "tipo_logradouro": "Travessa",
+            "bairro": "Lake Danielle",
+            "cidade": "Judymouth",
+            "cep": "85184",
+            "tipo": "casa",
+            "valor": 488423.52,
+            "data_aquisicao": "2017-07-29"
+        }
+    ]
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT * FROM imoveis WHERE cidade = %s",
+        ("Judymouth",)
+    )
+
+    mock_cursor.fetchall.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
+
+@patch("api.conectar_banco")
+def test_buscar_imoveis_cidade_erro(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_conectar_banco.return_value = mock_conn
+    mock_cursor.fetchall.return_value = []
+
+    response = client.get("/imoveis?cidade=seoul")
+    assert response.status_code == 200
+    assert response.get_json() == []
+
+    mock_cursor.execute.assert_called_once_with(
+        "SELECT * FROM imoveis WHERE cidade = %s",
+        ("seoul",)
+    )
+
     mock_cursor.fetchall.assert_called_once()
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
